77cd477b3804c7f5518135e24287525ed63bd082
Luthfi A. Ardhani <luthfidhani17@gmail.com>
2024-08-12 18:15:16 +0700

    ⚙️ fix: allow CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.routers import members, periods
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(members.router, prefix="/member", tags=["members"])
 app.include_router(periods.router, prefix="/period", tags=["periods"])
